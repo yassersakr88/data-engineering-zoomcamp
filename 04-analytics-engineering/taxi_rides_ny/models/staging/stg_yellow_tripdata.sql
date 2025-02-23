1932579da9a7f7e57a24f43f1b80859801a56ea1
Yasser Sakr <yasser.mansour88@gmail.com>
2025-02-23 19:13:05 +0000

    commit

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
@@ -11,9 +11,9 @@ select
    -- identifiers
     {{ dbt_utils.generate_surrogate_key(['vendorid', 'tpep_pickup_datetime']) }} as tripid,    
     {{ dbt.safe_cast("vendorid", api.Column.translate_type("integer")) }} as vendorid,
-    {{ dbt.safe_cast("ratecodeid", api.Column.translate_type("integer")) }} as ratecodeid,
-    {{ dbt.safe_cast("pulocationid", api.Column.translate_type("integer")) }} as pickup_locationid,
-    {{ dbt.safe_cast("dolocationid", api.Column.translate_type("integer")) }} as dropoff_locationid,
+    {{ dbt.safe_cast("rate_code", api.Column.translate_type("integer")) }} as ratecodeid,
+    {{ dbt.safe_cast("pickup_location_id", api.Column.translate_type("integer")) }} as pickup_locationid,
+    {{ dbt.safe_cast("dropoff_location_id", api.Column.translate_type("integer")) }} as dropoff_locationid,
 
     -- timestamps
     cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
