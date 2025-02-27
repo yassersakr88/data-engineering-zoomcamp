7e6c672f7fc6bf1a03aba34f9d38efcfb3ada685
Yasser Sakr <yasser.mansour88@gmail.com>
2025-02-27 21:37:49 +0000

    commit

--- 04-analytics-engineering/week4_hw/models/staging/stg_fhv_tripdata.sql
+++ 04-analytics-engineering/week4_hw/models/staging/stg_fhv_tripdata.sql
@@ -7,39 +7,24 @@
 with tripdata as 
 (
   select *,
-    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
+    row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
   from {{ source('staging','fhv_tripdata') }}
-  where vendorid is not null 
+  where dispatching_base_num is not null 
 )
 select
     -- identifiers
-    {{ dbt_utils.generate_surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
-    {{ dbt.safe_cast("vendorid", api.Column.translate_type("integer")) }} as vendorid,
-    {{ dbt.safe_cast("ratecodeid", api.Column.translate_type("integer")) }} as ratecodeid,
+    {{ dbt_utils.generate_surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+    {{ dbt.safe_cast("dispatching_base_num", api.Column.translate_type("integer")) }} as dis_base_num,
     {{ dbt.safe_cast("pulocationid", api.Column.translate_type("integer")) }} as pickup_locationid,
     {{ dbt.safe_cast("dolocationid", api.Column.translate_type("integer")) }} as dropoff_locationid,
-    
+    {{ dbt.safe_cast("affiliated_base_number", api.Column.translate_type("integer")) }} as aff_base_num,
+
     -- timestamps
-    cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
-    cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropoff_datetime as timestamp) as dropoff_datetime,
     
     -- trip info
-    store_and_fwd_flag,
-    {{ dbt.safe_cast("passenger_count", api.Column.translate_type("integer")) }} as passenger_count,
-    cast(trip_distance as numeric) as trip_distance,
-    {{ dbt.safe_cast("trip_type", api.Column.translate_type("integer")) }} as trip_type,
-
-    -- payment info
-    cast(fare_amount as numeric) as fare_amount,
-    cast(extra as numeric) as extra,
-    cast(mta_tax as numeric) as mta_tax,
-    cast(tip_amount as numeric) as tip_amount,
-    cast(tolls_amount as numeric) as tolls_amount,
-    cast(ehail_fee as numeric) as ehail_fee,
-    cast(improvement_surcharge as numeric) as improvement_surcharge,
-    cast(total_amount as numeric) as total_amount,
-    coalesce({{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }},0) as payment_type,
-    {{ get_payment_type_description("payment_type") }} as payment_type_description
+    sr_flag,
 from tripdata
 where rn = 1
 
