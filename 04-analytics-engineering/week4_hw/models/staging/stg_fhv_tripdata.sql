f9bc09fc52b72da51b8925873d9898d7afffbfc7
Yasser Sakr <yasser.mansour88@gmail.com>
2025-03-01 20:20:29 +0000

    sync

---
+++ 04-analytics-engineering/week4_hw/models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,32 @@
+{{ config(materialized='view') }}
+ 
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
+  from {{ source('staging','fhv_tripdata') }}
+  where dispatching_base_num is not null 
+)
+select
+   -- identifiers
+    {{ dbt_utils.generate_surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,    
+    {{ dbt.safe_cast("dispatching_base_num", api.Column.translate_type("STRING")) }} as disp_base_num,
+    {{ dbt.safe_cast("PUlocationID", api.Column.translate_type("integer")) }} as pickup_locationid,
+    {{ dbt.safe_cast("DOlocationID", api.Column.translate_type("integer")) }} as dropoff_locationid,
+
+    -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropOff_datetime as timestamp) as dropoff_datetime,
+    
+    -- trip info
+    SR_Flag,
+    {{ dbt.safe_cast("Affiliated_base_number", api.Column.translate_type("STRING")) }} as Aff_base_number
+from tripdata
+where rn = 1
+
+-- dbt build --select <model.sql> --vars '{'is_test_run: false}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
